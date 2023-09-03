41be0a39387779910006db5a84609d3e0b206493
Tehran <tehran.alishov@gmail.com>
2023-09-03 17:13:10 +0400

    Promotions set up

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,14 +2,46 @@
 
 namespace App\Controller;
 
+use App\DTO\LowestPriceEnquiry;
+use App\Filter\PromotionsFilterInterface;
+use App\Service\Serializer\DTOSerializer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProductController extends AbstractController
 {
-    #[Route('/product', name: 'app_product')]
-    public function index(): JsonResponse
+    #[Route('/products/{id}/lowest-price', name: 'lowest_price', methods: 'POST')]
+    public function lowestPrice(Request $request, int $id, DTOSerializer $serializer, PromotionsFilterInterface $promotionsFilter): Response
+    {
+        if ($request->headers->has('force_fail')) {
+            return new JsonResponse(
+                ['error' => 'Promotions Engine failure message'],
+                $request->headers->get('force_fail')
+            );
+        }
+
+        // 1. Deserialize json data into an EnquiryDTO
+        /** @var LowestPriceEnquiry $lowestPriceEnquiry */
+        $lowestPriceEnquiry = $serializer->deserialize($request->getContent(), LowestPriceEnquiry::class, 'json');
+
+        // 2. Pass the Enquiry into a promotions filter
+        // the appropriate promotion will be applied
+        $modifiedEnquiry = $promotionsFilter->apply($lowestPriceEnquiry);
+
+        // 3. Return the modified Enquiry
+        $responseContent = $serializer->serialize($modifiedEnquiry, 'json');
+
+        return new Response($responseContent, Response::HTTP_OK, ['Content-Type' => 'application/json']);
+
+//        return new JsonResponse($lowestPriceEnquiry);
+//        return $this->json($lowestPriceEnquiry);
+    }
+
+    #[Route('/products/{id}/promotions', name: 'promotions', methods: 'GET')]
+    public function promotions(): JsonResponse
     {
         return $this->json([
             'message' => 'Welcome to your new controller!',
