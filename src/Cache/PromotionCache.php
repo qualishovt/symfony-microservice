e7c2bf89c57e831b47a8ed5742e4f9891d99a3b2
Tehran <tehran.alishov@gmail.com>
2023-09-05 12:13:47 +0400

    Redis setup

---
+++ src/Cache/PromotionCache.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Cache;
+
+use App\Entity\Product;
+use App\Repository\PromotionRepository;
+use Psr\Cache\InvalidArgumentException;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
+
+class PromotionCache
+{
+    public function __construct(private CacheInterface $cache, private PromotionRepository $repository)
+    {
+    }
+
+    /**
+     * @throws InvalidArgumentException
+     */
+    public function findValidForProduct(Product $product, string $requestDate): ?array
+    {
+        $key = sprintf("find-valid-for-product-%d", $product->getId());
+
+        return $this->cache->get(
+            $key,
+            function (ItemInterface $item) use ($product, $requestDate) {
+                $item->expiresAfter(5);
+
+                var_dump('miss');
+
+                return $this->repository->findValidForProduct(
+                    $product,
+                    date_create_immutable($requestDate)
+                );
+            }
+        );
+    }
+}
--- src/DTO/PromotionEnquiryInterface.php
+++ src/DTO/PromotionEnquiryInterface.php
@@ -6,4 +6,9 @@ use App\Entity\Product;
 
 interface PromotionEnquiryInterface
 {
+    public function getProduct(): ?Product;
+
+    public function setPromotionId(int $promotionId);
+
+    public function setPromotionName(string $promotionName);
 }
